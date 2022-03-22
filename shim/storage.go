ceebd06f6efd13bda282c0a843d12f62d51fdcf4
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-22 05:12:00 +0100

    storage for customerID pdf

---
+++ shim/storage.go
@@ -0,0 +1,18 @@
+package shim
+
+import (
+	"bytes"
+	"strconv"
+)
+
+func main()  {
+
+	err = stub.Putstate(first, []byte(strconv.Itoa(data)))
+	if err != nil {
+		return Error(err.Error())
+	}
+}
+
+func ()  {
+	
+}
