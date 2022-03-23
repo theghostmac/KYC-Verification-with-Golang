fd4017754794b469ed95d53dfad5c8b04b9ea644
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-23 09:15:50 +0100

    storage fixture

--- shim/storage.go
+++ shim/storage.go
@@ -1,18 +1,105 @@
 package shim
-
 import (
 	"bytes"
+	"os"
 	"strconv"
+	"strings"
+	"testing"
+	"github.com/hyperledger/fabric/common/flogging"
+	mockpeer "github.com/hyperledger/fabric/common/mocks/peer"
+	"github.com/hyperledger/fabric/common/util"
+	lproto "github.com/hyperledger/fabric/protos/ledger/queryresult"
+	pb "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric/protos/utils"
 )
 
 func main()  {
-
-	err = stub.Putstate(first, []byte(strconv.Itoa(data)))
+	err = stub.PutState(A, []byte(strconv.Itoa(data)))
 	if err != nil {
 		return Error(err.Error())
 	}
+
 }
 
-func ()  {
-	
+
+func (t *shimTestCC) historyq(stub ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) < 1 {
+		return Error("Incorrect number of arguments. Expecting 1")
+	}
+	id := args[0]
+	resultsIterator, err := stub.GetHistoryForKey(id)
+	if err != nil {
+		return Error(err.Error())
+	}
+	defer resultsIterator.Close()
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+		buffer.WriteString(", \"Value\":")
+		if response.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(response.Value))
+		}
+		buffer.WriteString(", \"IsDelete\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(strconv.FormatBool(response.IsDelete))
+		buffer.WriteString("\"")
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+	return Success(buffer.Bytes())
 }
+
+func (t *shimTestCC) richq(stub ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return Error("Incorrect number of arguments. Expecting keys for range query")
+	}
+	id := args[0]
+	resultsIterator, err := stub.GetQueryResult(query)
+	if err != nil {
+		return Error(err.Error())
+	}
+	defer resultsIterator.Close()
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return Error(err.Error())
+		}
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+		buffer.WriteString(", \"Record\":")
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+	return Success(buffer.Bytes())
+}
+
+func (t *shimTestCC) historyq(stub ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) < 1 {
+		return Error("Incorrect number of arguments. Expecting 1")
+	}
